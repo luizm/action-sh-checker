aaa5338b3bea905f77407d254012d2f368ff3823
luizm <luizmuller.it@gmail.com>
2019-11-20 15:17:02 -0300

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 cd "$GITHUB_WORKSPACE" || exit 1
-find . -type f -name \*.sh -exec shellcheck {} +
 
-bash -c "shfmt --version"
+shellcheck $(find . -type f -name '*.sh' -not -path '*/.terraform/*') || exit_code="1"
 
 diverging_files="$(shfmt -d $(find . -type f -name '*.sh'))"
 if [ -n "$diverging_files" ]; then
   echo "$diverging_files"
 	echo -e "The files above have some formatting problems, you can use shfmt -w to fix them:\n"
-	exit 1
+	exit_code="1"
 fi
+
+exit "${exit_code:-0}"
