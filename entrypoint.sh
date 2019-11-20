6d58b6e6bc15abc06c16fb0cf5659c32d8dfd6b1
luizm <luizmuller.it@gmail.com>
2019-11-20 17:51:32 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 cd "$GITHUB_WORKSPACE" || exit 1
 
 echo -e "Validating shell scripts files using shellcheck\n"
-shellcheck $(find . -type f -name '*.sh' | grep -Ev "$SH_CHECKER_EXCLUDE_REGEX") || {
+shellcheck $(find . -type f -name '*.sh' -not -regex "$SH_CHECKER_EXCLUDE_REGEX") || {
 	echo -e "\nThe files above have some shellcheck issues\n"
 	exit_code="1"
 }
 
 echo -e "Validating shell scripts files using shfmt\n"
-diverging_files="$(shfmt -d $(find . -type f -name '*.sh' | grep -Ev "$SH_CHECKER_EXCLUDE_REGEX"))"
+diverging_files="$(shfmt -d $(find . -type f -name '*.sh' -not -regex "$SH_CHECKER_EXCLUDE_REGEX"))"
 if [ -n "$diverging_files" ]; then
   echo "$diverging_files"
 	echo -e "\nThe files above have some formatting problems, you can use shfmt -w to fix them\n"
