00fcc4499492bb7e944a5823f9ce0fe07d6ef8cb
luizm <luizmuller.it@gmail.com>
2019-11-20 14:25:12 -0300

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 cd "$GITHUB_WORKSPACE" || exit 1
-find . -name \*.sh -exec shellcheck {} +
-find . -name \*.sh -exec shfmt {} +
+find . -type f -name \*.sh -exec shellcheck {} +
+
+diverging_files="$(shfmt -d $(find . -type f -name '*.sh'))"
+if [ -n "$diverging_files" ]; then
+	echo "$diverging_files"
+	echo -e "The files above have some formatting problems, you can use shfmt -w to fix them:\n"
+	exit 1
+fi
