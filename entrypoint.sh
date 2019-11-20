d36f9395b7ce83a6e94c413f95dbf1ea655e5c0d
luizm <luizmuller.it@gmail.com>
2019-11-20 17:25:43 -0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 cd "$GITHUB_WORKSPACE" || exit 1
 
-echo -e "Validating shell scripts files using shellcheck:\n"
-shellcheck $(find . -type f -name '*.sh' | grep -Ev \"$SH_CHECKER_EXCLUDE_REGEX\") || {
+echo -e "Validating shell scripts files using shellcheck\n"
+shellcheck $(find . -type f -name '*.sh' | grep -Ev "$SH_CHECKER_EXCLUDE_REGEX") || {
 	echo -e "\nThe files above have some shellcheck issues\n"
 	exit_code="1"
 }
 
-echo -e "Validating shell scripts files using shfmt:\n"
-diverging_files="$(shfmt -d $(find . -type f -name '*.sh' | grep -Ev \"$SH_CHECKER_EXCLUDE_REGEX\"))"
+echo -e "Validating shell scripts files using shfmt\n"
+diverging_files="$(shfmt -d $(find . -type f -name '*.sh' | grep -Ev "$SH_CHECKER_EXCLUDE_REGEX"))"
 if [ -n "$diverging_files" ]; then
   echo "$diverging_files"
 	echo -e "\nThe files above have some formatting problems, you can use shfmt -w to fix them\n"
@@ -17,7 +17,7 @@ if [ -n "$diverging_files" ]; then
 fi
 
 if [ -z "$exit_code" ]; then
-	echo -e "\nAll sh files found looks fine :)\n"
+	echo -e "All sh files found looks fine :)\n"
 fi
 
 exit "${exit_code:-0}"
