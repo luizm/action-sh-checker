39edfdc42fccfc1dc6dee2c8ae162addfdfcc2c0
luizm <luizmuller.it@gmail.com>
2019-11-20 15:33:02 -0300

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 
 cd "$GITHUB_WORKSPACE" || exit 1
 
-shellcheck $(find . -type f -name '*.sh' -not -path '*/.terraform/*') || exit_code="1"
+echo "Validating shell scripts files using shellcheck:"
+shellcheck $(find . -type f -name '*.sh' -not -path \'$EXCLUDE_REGEX\') || exit_code="1"
 
-diverging_files="$(shfmt -d $(find . -type f -name '*.sh'))"
+echo "Validating shell scripts files using shfmt:"
+diverging_files="$(shfmt -d $(find . -type f -name '*.sh' -not -path \'$EXCLUDE_REGEX\'))"
 if [ -n "$diverging_files" ]; then
   echo "$diverging_files"
 	echo -e "The files above have some formatting problems, you can use shfmt -w to fix them:\n"
