035121b3e8d3af2171b63e7f929826de137da61f
Ross Smith II <ross@smithii.com>
2023-10-24 09:22:59 -0700

    Reformat via shfmt

--- upgrade.sh
+++ upgrade.sh
@@ -16,9 +16,9 @@ GH_VER=$(${dl} https://api.github.com/repos/cli/cli/tags | jq -r '.[0].name')
 SHELLCHECK_VER=$(${dl} https://api.github.com/repos/koalaman/shellcheck/tags | jq -r '.[0].name')
 SHFMT_VER=$(${dl} https://api.github.com/repos/mvdan/sh/tags | jq -r '.[0].name')
 
-GH_VER="${GH_VER/v}"
-SHELLCHECK_VER="${SHELLCHECK_VER/v}"
-SHFMT_VER="${SHFMT_VER/v}"
+GH_VER="${GH_VER/v/}"
+SHELLCHECK_VER="${SHELLCHECK_VER/v/}"
+SHFMT_VER="${SHFMT_VER/v/}"
 
 sed -Ei "
 s/^(FROM\s+alpine:).*/\1${ALPINE_VER}/;
